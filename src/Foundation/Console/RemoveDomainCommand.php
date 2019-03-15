89ebe58323d3eeeb1ef330810b6fa8e0540af675
muhammad <buruhsd@outlook.co.id>
2019-03-15 14:45:30 +0700

    Remove domain and db

--- src/Foundation/Console/RemoveDomainCommand.php
+++ src/Foundation/Console/RemoveDomainCommand.php
@@ -61,7 +61,15 @@ class RemoveDomainCommand extends GeneratorCommand {
 
     protected function deleteDomainEnvFile()
     {
+
         $domainEnvFilePath = $this->getDomainEnvFilePath();
+        $envDomainArray = $this->getVarsArray($domainEnvFilePath);
+        //Get user
+        $envFilePath = base_path() . DIRECTORY_SEPARATOR . Config::get('domain.env_stub', '.env');
+        $envArray = $this->getVarsArray($envFilePath);
+
+        $this->removeDomainDatabase($envDomainArray['DB_DATABASE'], $envArray);
+
         if ($this->files->exists($domainEnvFilePath)) {
             $this->files->delete($domainEnvFilePath);
         }
@@ -81,6 +89,23 @@ class RemoveDomainCommand extends GeneratorCommand {
         }
         $config['domains'] = $domains;
         return $config;
+    }
+
+    public function removeDomainDatabase($dbname, $dbconn){
+        $conn = new \mysqli($dbconn['DB_HOST'], $dbconn['DB_USERNAME'], $dbconn['DB_PASSWORD']);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        // Create database
+        $sql = "DROP DATABASE ".$dbname;
+        if ($conn->query($sql) === TRUE) {
+            echo "Database remove successfully";
+        } else {
+            echo "Error creating database: " . $conn->error;
+        }
+
+        $conn->close();
     }
 
 
